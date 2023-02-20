0e52a8cdf09d09c249a0331edf1b18ab4736f3aa
MaxLyra <maxwell.moraes@digitalpages.com.br>
2023-02-20 12:12:47 -0300

    (ADD) Project hr-worker created

---
+++ hr-worker/src/test/java/com/tech/max/hrWorker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.max.hrWorker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
